fd44381cd177c2ad7d3aa5bc3aaced723ad82300
harshini10953 <harshini.r1112@gmail.com>
2025-12-01 09:17:28 +0530

    Add files via upload

---
+++ SumOfFirstNNaturalNumbers.java
@@ -0,0 +1,13 @@
+public class SumOfFirstNNaturalNumbers {
+    public static void main(String[] args) {
+
+        int N = 10; 
+        int sum = 0;
+
+        for (int i = 1; i <= N; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum of first " + N + " natural numbers = " + sum);
+    }
+}
